d8716d0970af8825b6e0052584e11daaefc3b448
26blevine <26blevine@gannacademy.org>
2025-09-02 15:16:20 -0400

    oops lost in the sauce

--- src/create_tuple.py
+++ src/create_tuple.py
@@ -1,6 +1,12 @@
 # Write your solution here
 def create_tuple(x: int, y: int, z: int):
-    pass
+    nums = [x, y, z]
+
+    nx = min(nums)
+    ny = max(nums)
+    nz = sum(nums)
+
+    return (nx, ny, nz)
 
 
 if __name__ == "__main__":
